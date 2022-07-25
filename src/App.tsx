88e45b29457bc352d45b6b0d0c90e8dc6f565e0c
KarlGilden <karl.gildenhuys@gmail.com>
2022-07-25 14:55:57 +1200

    add cocktail api

--- src/App.tsx
+++ src/App.tsx
@@ -2,57 +2,65 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import { Pokemon } from 'pokenode-ts';
+import { isValidDateValue } from '@testing-library/user-event/dist/utils';
 
 function App() {
-  const [pokemonName, setPokemonName] = useState("");
-  const [pokemonInfo, setPokemonInfo] = useState<undefined | Pokemon >(undefined);
+  const [cocktailData, setCocktailData] = useState<undefined | any >(undefined);
+  const [alcoholType, setAlcoholType] = useState("undefined");
+  const [searchQuery, setSearchQuery] = useState("");
 
-  const POKEMON_BASE_URL = "https://pokeapi.co/api/v2";
+
+  const COCKTAILDB_BASEURL = "https://www.thecocktaildb.com/api/json/v1/1/";
 
   const search = () => {
-    axios.get(POKEMON_BASE_URL + "/pokemon/" + pokemonName).then((res) => {
+    axios.get(COCKTAILDB_BASEURL + "search.php?s=" + searchQuery).then((res) => {
       console.log(res.data);
-      setPokemonInfo(res.data)
+      setCocktailData(res.data)
     });
 
   }
   return (
-    <div className='p-6'>
+    <div className='p-6 '>
       <h1 className="text-3xl font-bold my-3">
-        Pokemon Search
+        The Cocktail Bar
       </h1>
       
       <div>
-        <label className=''>Pokemon Name</label>
-
-        <br/>
         <input 
           type="text" 
-          id="pokemon-name" 
-          name="pokemon-name" 
-          className='border-[1px] border-black px-2 my-2'
-          onChange={e => setPokemonName(e.target.value)}
-          />
-        <br/>
+          className='border-[1px] border-black my-2'
+          onChange={(e)=>setSearchQuery(e.target.value)}
+        />
 
         <button className='border-[1px] border-black my-2' onClick={search}>
         Search
         </button>
       </div>
 
-      <p>
-        You have entered {pokemonName}
-      </p>
-
-      {pokemonInfo === undefined ? (
+      {cocktailData === undefined ? (
         <p></p>
       ) : (
-        <div id="pokemon-result">
-          <p>Name: {pokemonInfo.name}</p>
-          <p>Height: {pokemonInfo.height}</p>
-          <p>Weight: {pokemonInfo.weight}</p>
-          {pokemonInfo?.sprites.other.dream_world.front_default ? <img src={pokemonInfo.sprites.other.dream_world.front_default} alt="" /> : <p>Cannot find image</p>}
+        <div className=''>
+          {cocktailData.drinks.map((value:any)=>{
+            return(
+              <div key={value.idDrink} className='p-3 mb-[50px] w-full max-w-[800px]'>
+                <div className='flex flex-col md:flex-row justify-between items-start mb-[25px]'>
+                  <div className='mr-3'>
+                    <h1 className='text-5xl'>{value.strDrink}</h1>
+                    <br />
+                    <p className='italic'>{value.strIngredient1}</p>
+                    <p className='italic'>{value.strIngredient2}</p>
+                    <p className='italic'>{value.strIngredient3}</p>
+                    <p className='italic'>{value.strIngredient4}</p>
+                    <br />
+                  </div>
+                  <img className='w-full sm:w-[50%] md:w-[25%]' src={value.strDrinkThumb} alt="" />
+                </div>
+                <p>{value.strInstructions}</p>
+
+              </div>
+            )
+          })}
         </div>
       )}
 
